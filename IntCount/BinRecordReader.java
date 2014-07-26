b6ab1fd2da49e28a16f6d97ee59bca9599165afa
betatheta <liangyang.g@gmail.com>
2014-07-26 19:02:04 +0800

    new file:   IntCount/BinInputFormat.java 	new file:   IntCount/BinRecordReader.java 	new file:   IntCount/IntCount.java 	new file:   IntCount/README.md

---
+++ IntCount/BinRecordReader.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+/**
+ * Treats keys as offset in file and value as line. 
+ */
+public class BinRecordReader extends RecordReader<LongWritable, Text> {
+
+  private long start;
+  private long pos;
+  private long end;
+  private FSDataInputStream in;
+  private LongWritable key = null;
+  private Text value = null;
+
+  public void initialize(InputSplit genericSplit,
+                         TaskAttemptContext context) throws IOException {
+    FileSplit split = (FileSplit) genericSplit;
+    Configuration job = context.getConfiguration();
+    start = split.getStart();
+    end = start + split.getLength();
+    final Path file = split.getPath();
+    FileSystem fs = file.getFileSystem(job);
+    this.in = fs.open(split.getPath());
+    in.seek(start);
+    this.pos = start;
+  }
+  
+  public boolean nextKeyValue() throws IOException {
+    if (key == null) {
+      key = new LongWritable();
+    }
+    key.set(pos);
+    if (value == null) {
+      value = new Text();
+    }
+    if (pos < end) {
+      value.set(Integer.toString(Integer.reverseBytes(in.readInt())));
+      pos = in.getPos();
+      return true;
+    }else{
+    	return false;
+    }
+
+  }
+
+  @Override
+  public LongWritable getCurrentKey() {
+    return key;
+  }
+
+  @Override
+  public Text getCurrentValue() {
+    return value;
+  }
+
+  /**
+   * Get the progress within the split
+   */
+  public float getProgress() {
+    if (start == end) {
+      return 0.0f;
+    } else {
+      return Math.min(1.0f, (pos - start) / (float)(end - start));
+    }
+  }
+  
+  public synchronized void close() throws IOException {
+    if (in != null) {
+      in.close(); 
+    }
+  }
+}
---
+++ IntCount/README.md
@@ -0,0 +1,4 @@
+This project aims to learn TextInputFormat class.
+Read 4 bytes integer from a little endian binary file.
+Count the numbers of each integer.
+The output is a text file.
